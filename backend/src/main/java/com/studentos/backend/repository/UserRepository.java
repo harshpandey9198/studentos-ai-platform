03c55f47d681aff456661b4c30e35e3706e938af
Harsh pandey <harshpandey9989@gmail.com>
2026-05-21 13:19:41 +0530

    Deleted UserRepository

--- backend/src/main/java/com/studentos/backend/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.studentos.backend.repository;
-
-import com.studentos.backend.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
-    User findByEmail(String email);
-}
